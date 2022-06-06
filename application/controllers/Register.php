4c38109e7f73a75f30b66628e213c8165ce0d904
Alice <kiroro.christianne@gmail.com>
2022-06-07 03:42:24 +0800

    Added home controller

---
+++ application/controllers/Register.php
@@ -0,0 +1,12 @@
+<?php
+
+class Register extends CI_Controller {
+    
+    public function index() {
+        $this->load->view('register.php');
+    }
+
+    public function register() {
+    }
+}
+?>
