8a1a9ae4765ae00233d59ecf49ca325db72e5ce0
MakuAren <69324542+MakuAren@users.noreply.github.com>
2022-06-07 09:21:08 +0800

    item select

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,10 +1,46 @@
 <?php
 
 class Home extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+    }
 
     public function index() {
         $this->load->helper('url');
         $this->load->view('templates/header');
-        $this->load->view('home');
+        $product["productList"] = array("","","","","","","","");
+        if(empty($this->session->userdata('order'))) {
+            $order = array();
+            for ($x = 0; $x <= count($product["productList"]); $x++) {
+                $order[$x] = 0;
+            }
+
+            $this->session->userdata('order');
+            $this->session->set_userdata('order',$order);
+        }
+
+        $this->load->view('home',$product);
+    }
+
+    public function placeOrder() {
+        header('content-type: text/json');
+        if (!isset($_POST['pid']) || !isset($_POST['bs'])) {
+            exit;
+        }
+        $bs = $_POST['bs'];
+        $order = $this->session->userdata('order');
+        if($bs == "1") {
+            $bs = 0;
+        } else {
+            $bs = 1;
+        }
+
+        $order[$_POST['pid']] = $bs;
+        $this->session->set_userdata('order', $order);
+        
+
+        echo json_encode($bs);
     }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,6 @@
+<?php
+$order = $this->session->userdata('order');
+?>
 <body>    
     <nav id="header" class="navbar navbar-expand-lg">
         <div class="container-fluid">
@@ -15,18 +18,43 @@
     </nav>
 
     <div id="menu">
-        <?php for ($x = 0 ; $x < 8; $x++): ?>
-        <div class="xcard">
-            <!-- image placeholder -->
-            <img src="<?= base_url('imgs/BaTKgtJF_4x.jpg');?>" alt="">
-            <div class="xcard-details">
-                <p class='xcard-title'>chixsilog</p>
-                <p class='xcard-price mb-0'>&#8369 80</p>
+        <?php $xx = 0; foreach($productList as $x){ ?>
+            <?php if($order[$xx] == 1){?>
+                <div class="xcard"  id="productBanner<?=$xx;?>" style="background-color:rgb(25,135,84);" onclick="order(<?=$xx;?>)">
+            <?php } else {?>
+                <div class="xcard"  id="productBanner<?=$xx;?>" style="background-color:rgb(244,162,97);" onclick="order(<?=$xx;?>)">
+            <?php }?>
+                <input type="number" id="bannerStatus<?=$xx;?>" value=<?=$order[$xx]?> hidden>
+                <!-- image placeholder -->
+                <?php if($order[$xx] == 1){?>
+                    <img class="orderClick" style="transition: opacity .5s; opacity: 0.5;" id="bannerImg<?=$xx;?>" src="<?= base_url('imgs/BaTKgtJF_4x.jpg');?>" alt="">
+                <?php } else {?>
+                    <img class="orderClick" style="transition: opacity .5s; opacity: 1;" id="bannerImg<?=$xx;?>" src="<?= base_url('imgs/BaTKgtJF_4x.jpg');?>" alt="">
+                <?php }?>
+                <div class="xcard-details">
+                    <p class='xcard-title'>chixsilog</p>
+                    <p class='xcard-price mb-0'>&#8369 80</p>
+                </div>
             </div>
-        </div>
-        <?php endfor; ?>
+        <?php $xx++; }?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script>
+        function order(id){
+            bStatus = document.getElementById("bannerStatus"+id).value;
+            $.post('<?=base_url('PlaceOrder');?>', {pid: id, bs: bStatus}, function(data){
+                if(data == "0"){
+                    document.getElementById("productBanner"+id).style.backgroundColor = "rgb(244,162,97)";
+                    document.getElementById("bannerStatus"+id).value = 0;
+                    document.getElementById("bannerImg"+id).style.opacity = "1";
+                }else{
+                    document.getElementById("productBanner"+id).style.backgroundColor = "rgb(25,135,84)";
+                    document.getElementById("bannerStatus"+id).value = 1;
+                    document.getElementById("bannerImg"+id).style.opacity = "0.5";
+                }
+            }, 'JSON');
+        }
+    </script>
 
 </body>
