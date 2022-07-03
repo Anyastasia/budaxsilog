b460d8c798cecc87e1eee3478311d2ebd0c7b7b2
Alice <kiroro.christianne@gmail.com>
2022-07-03 19:26:23 +0800

    Added admin controller and view

---
+++ application/views/admin.php
@@ -0,0 +1,10 @@
+<body>
+    <div class="container v-100 flex flex-center">
+        <div class="w-40">
+            <form action="admin-login.php">
+                <p>Enter password</p>
+                <input type="password" name="password" id="password">
+            </form>
+        </div>
+    </div>
+</body>
